2ad5843804b233ddf4d206457d3276444b3a8621
melifaroo <den.volgin@gmail.com>
2026-08-15 15:26:21 +0200

    before restructure to multiple project soution

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@
 Console.WriteLine("This app helps to group files to folder by filename template");
 Console.WriteLine("IMG_YYYYMMDD_******.jpg -> /YYYY/YYYY-MM/IMG_YYYYMMDD_******.jpg");
 
-String pattern = @"^IMG_(\d{4})(\d{2})(\d{2})_.*\.jpg$";
-String sourcePath = Path.Join(@"D:\photo\Redmi Note 10 Pro 2023\Photo");
+String pattern = @"^(IMG|PANO|VID)[-_](\d{4})(\d{2})(\d{2})[-_](.{2}).*\.(jpg|mp4)$";
+String sourcePath = Path.Join(@"D:\photo\2019");
 String subfolder;
 
 int count = 0;
@@ -14,13 +14,20 @@ foreach(var file in Directory.GetFiles(sourcePath)){
     string filename = Path.GetFileName(file);
     var match = Regex.Match(filename, pattern);
     subfolder = ( match.Success )
-        ?Path.Join(sourcePath, match.Groups[1].Value, String.Join("-", match.Groups[1].Value, match.Groups[2].Value) )
+        ?Path.Join(sourcePath,
+        match.Groups[2].Value, 
+        match.Groups[1].Value, 
+        (match.Groups[5].Value=="WA")?"WhatsApp":"",  
+        String.Join("_", 
+            match.Groups[2].Value, 
+            match.Groups[3].Value, 
+            match.Groups[4].Value) )
         :Path.Join(sourcePath, "ungroupped" );
     
     if (!Directory.Exists(subfolder))
         Directory.CreateDirectory(subfolder);
-    Console.WriteLine(count +"\t"+ filename+"\tcopied to "+subfolder); 
     File.Copy(file, Path.Combine(subfolder, filename));
 
-    if (++count>limit) break;
+    Console.WriteLine(++count +"\t"+ filename+"\tcopied to "+subfolder); 
+    if (count>limit) break;
 }
